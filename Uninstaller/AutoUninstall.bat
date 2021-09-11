70f0ccd795b91a51d21ce1e80633b1c1c9c2fd84
Joseph Y <josephy@dram.space>
2021-09-11 15:56:15 -0500

    tabs are hard

--- Uninstaller/AutoUninstall.bat
+++ Uninstaller/AutoUninstall.bat
@@ -2,7 +2,7 @@
 SETLOCAL EnableDelayedExpansion
 
 set regpath=HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall
-set SoftName=
+set SoftName=vc
 reg query "%regpath%" /f "%SoftName%" /s 2>&1>nul
 if %ERRORLEVEL% EQU 0 (
     for /f "tokens=1,2*" %%a in ('reg query "%regpath%" /s /d /f "%SoftName%"') do (
@@ -14,20 +14,20 @@ if %ERRORLEVEL% EQU 0 (
                 set Uninstall=%%e
                 ECHO ####CHECKING UNINSTALL####
                 if /i "!Uninstall:~0,3!" EQU "Msi" (
-                    ECHO MsiExec uninstallation command found!
-                    ECHO Uninstall String: !Uninstall!
-                    ECHO Trying silent MSI uninstallation.
-			        !uninstall!
-		            ECHO Uninstall complete.
-                        ) ELSE (
-                    ECHO Exe uninstallation command found!
-                    ECHO UninstallString: !Uninstall!
-                    ECHO Trying silent .exe uninstallation 
-                    !uninstall! /s
-		            ECHO Uninstall complete.
-                        )
+                        ECHO MsiExec uninstallation command found!
+                        ECHO Uninstall String: !Uninstall!
+                        ECHO Trying silent MSI uninstallation.
+                        !uninstall!
+                        ECHO Uninstall complete.
+                            ) ELSE (
+                        ECHO Exe uninstallation command found!
+                        ECHO UninstallString: !Uninstall!
+                        ECHO Trying silent .exe uninstallation 
+                        !uninstall! /s
+                        ECHO Uninstall complete.
+                    )
             )
-        ) ELSE (
+            ) ELSE (
             set str=%%a
             if "!str:~0,4!"=="HKEY" set key=%%a
         )
