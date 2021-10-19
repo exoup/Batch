a707752cf4b86118b4136200750390a99c6e1914
Joseph Y <JosephY@dram.space>
2021-10-19 16:33:42 -0500

    Added new line between greeting and body. Tested working.

--- Email/PrefabEmailSend.bat
+++ Email/PrefabEmailSend.bat
@@ -1,18 +1,28 @@
 @ECHO OFF
 setlocal EnableDelayedExpansion
-set attachment=ATTACHMENT PATH HERE
-set subject=SUBJECT LINE HERE
+set attachment=C:\users\public\desktop\replacementpath.txt
+set subject=REPLACEMENT TEXT
 
 :start
 set /p name=Input your name: 
 set /p email=Input your email address: 
 cls
 
+REM For setting new line character in the email.
+(set \n=^
+%=empty, do not delete this line =%
+^
+%=empty, do not delete this line =%
+)
+
 ECHO Name: %name%
 ECHO Email Address: %email%
 choice /c yn /m "Is this correct?"
 if !ERRORLEVEL! EQU 1 (
-set body=Dear %name%, REST OF TEXT HERE
+set greeting=Dear %name%,
+REM Place email text after body below for it to be on a new line from the greeting. Otherwise, all one line.
+set body=Replacement text. ^
+This will be on the same line as the first part of the message.
 ECHO Opening Outlook.
 ) ELSE (
 if !ERRORLEVEL! EQU 2 (
@@ -20,8 +30,8 @@ cls
 goto :start
 )
 )
+REM Fill in path to your Outlook.exe here between the quotes.
+"C:\Program Files\Microsoft Office\root\Office16\Outlook.exe" /a "%attachment%" /c ipm.note /m "%email%?subject=%subject%&body=%greeting%!\n!%body%"
 
-"C:\Program Files\Microsoft Office\root\Office16\Outlook.exe" /a "%attachment%" /c ipm.note /m "%email%?subject=%subject%&body=%body%"
-
-ECHO Please check Outlook. && pause > NUL
+ECHO Please check Outlook. && pause
 exit /b
