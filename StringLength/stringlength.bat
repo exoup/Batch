b5877dc33cea9452f606ef6fc30ec1590536e2f5
Joseph Y <JosephY@dram.space>
2021-10-20 14:14:07 -0500

    Committed wrong thing because tired. String Lengthererer

--- StringLength/stringlength.bat
+++ StringLength/stringlength.bat
@@ -2,12 +2,13 @@
 
 set /p input=Enter some text: 
 set string=%input%#
-call :lengloop string
-ECHO %len%
+call :lengloop string strlen
+ECHO %strlen%
 EXIT /b
 
 :lengloop
 setlocal EnableDelayedExpansion
 :loop
 if NOT "!%1:~%len%!"=="#" set /a len+=1 & goto :loop
-endlocal & EXIT /b
+endlocal & set %2=%len%
+EXIT /b
